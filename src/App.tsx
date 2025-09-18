92d2ad21fac71e566de8f26f29e8b507076cd68b
fortunecloudtraining <fortunecloudtraining@gmail.com>
2025-09-18 13:25:27 +0530

    black

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
+    <div className="min-h-screen bg-black-100 p-6">
       <h1 className="text-3xl font-bold text-center mb-6">📚 Book Finder</h1>
 
       {/* Search */}
